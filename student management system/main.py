82cddb42e5506d3b6c165e70d1396939f83ac113
rasikasrimal <rasikacooldj620@gmail.com>
2023-09-28 14:36:39 +0530

    Changed Image Directory

--- student management system/main.py
+++ student management system/main.py
@@ -5,7 +5,7 @@ window = Tk()
 window.geometry('1280x700')
 
 # Load the background image
-background_image = ImageTk.PhotoImage(file=r'C:/Users/Rasika Srimal/Documents/GitHub/DBMS-MiniProjects/student management system/images/bg.jpg')
+background_image = ImageTk.PhotoImage(file='images/bg.jpg')
 
 # Create a label to display the background image
 bg_label = Label(window, image=background_image)
@@ -16,7 +16,7 @@ login_frame = Frame(window)
 login_frame.place(x=400, y=150)
 
 # Load the logo image
-logo_image = PhotoImage(file=r'C:/Users/Rasika Srimal/Documents/GitHub/DBMS-MiniProjects/student management system/images/logo.png')
+logo_image = PhotoImage(file='images/logo.png')
 
 # Create an image label to display the logo
 logo_label = Label(login_frame, image=logo_image)
