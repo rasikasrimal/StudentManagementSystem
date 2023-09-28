3c0ef4a2ca9a1d4864bcd49db90b2981e17fd9bf
rasikasrimal <rasikacooldj620@gmail.com>
2023-09-28 15:34:45 +0530

    cannot pass null for username and pass

--- student management system/main.py
+++ student management system/main.py
@@ -1,6 +1,13 @@
 from tkinter import *
-from PIL import ImageTk
+from PIL import ImageTk, Image
+from tkinter import messagebox
 
+# Function to handle login button click
+def login():
+    if usernameEntry.get() == '' or passwordEntry.get() == '':
+        messagebox.showerror('Error', 'All fields are required')
+
+# Create the main window
 window = Tk()
 window.geometry('1280x700')
 
@@ -12,14 +19,56 @@ bg_label = Label(window, image=background_image)
 bg_label.place(x=0, y=0)
 
 # Create the login frame
-login_frame = Frame(window)
+login_frame = Frame(window, bg='white')
 login_frame.place(x=400, y=150)
 
 # Load the logo image
-logo_image = PhotoImage(file='images/logo.png')
+logo_image = Image.open('images/logo.png')
+logo_photo = ImageTk.PhotoImage(logo_image)
 
 # Create an image label to display the logo
-logo_label = Label(login_frame, image=logo_image)
-logo_label.grid(row=0, column=0)
+logo_label = Label(login_frame, image=logo_photo)
+logo_label.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
 
+# Username
+usernameImage = PhotoImage(file='images/user.png')
+usernameLabel = Label(
+    login_frame, 
+    image=usernameImage, 
+    text='Username', 
+    compound=LEFT, 
+    font=('Helvetica', 20, 'bold')
+)
+usernameLabel.grid(row=1, column=0, padx=20, pady=10)
+
+usernameEntry = Entry(login_frame, font=('Helvetica', 20, 'bold'), bd=5, fg='royalblue', bg='lightgray')
+usernameEntry.grid(row=1, column=1, padx=20, pady=10)
+
+# Password
+passwordImage = PhotoImage(file='images/password.png')
+passwordLabel = Label(
+    login_frame, 
+    image=passwordImage, 
+    text='Password', 
+    compound=LEFT, 
+    font=('Helvetica', 20, 'bold')
+)
+passwordLabel.grid(row=2, column=0, padx=20, pady=10)
+
+passwordEntry = Entry(login_frame, font=('Helvetica', 20, 'bold'), bd=5, fg='royalblue', bg='lightgray', show='*')
+passwordEntry.grid(row=2, column=1, padx=20, pady=10)
+
+# Login Button
+LoginButton = Button(
+    login_frame, 
+    text='Login', 
+    font=('Helvetica', 20, 'bold'), 
+    width=15, 
+    fg='white', 
+    bg='cornflowerblue', 
+    command=login
+)
+LoginButton.grid(row=3, column=1, padx=20, pady=10)
+
+# Start the main loop
 window.mainloop()
